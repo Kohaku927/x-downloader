47600e45be303430c0207e6bc3c62dd26abb30bc
Kohaku927 <ik2007927@gmail.com>
2026-08-23 21:08:21 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -71,10 +71,6 @@ def download_file():
     return f"ダウンロードに失敗しました: {e}", 500
 
 
-if __name__ == "__main__":
-  app.run(debug=True, port=5000)
-  import os
-
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
